32d7bdaef258510dd8a995cdbcda66dda5df36bb
Flictivity <bulat_sa1@mail.ru>
2023-04-18 11:47:55 +0300

    added main page and authorization page markup added data backup script

---
+++ CinemaHermes/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CinemaHermes.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationBtnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbLogin.Text))
+            {
+                MessageBox.Show("Поле логин не заполнено!", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(pbPassword.Password))
+            {
+                MessageBox.Show("Поле пароль не заполнено!", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (chbRemember.IsChecked == true)
+            {
+                Properties.Settings.Default.Login = tbLogin.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbLogin.Text = Properties.Settings.Default.Login;
+        }
+    }
+}
---
+++ CinemaHermes/Pages/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CinemaHermes.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationBtnClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AuthorizationPage());
+        }
+    }
+}

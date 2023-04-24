73da039f76c3734e3ec61de3ae460bf595efa7c3
Flictivity <bulat_sa1@mail.ru>
2023-04-24 17:18:54 +0300

    fix bug

--- CinemaHermes/Pages/AuthorizationPage.xaml.cs
+++ CinemaHermes/Pages/AuthorizationPage.xaml.cs
@@ -19,56 +19,68 @@ namespace CinemaHermes.Pages
 
         private void AuthorizationBtnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(tbLogin.Text))
+            try
             {
-                MessageBox.Show("Поле логин не заполнено!", "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-            if (string.IsNullOrEmpty(pbPassword.Password))
-            {
-                MessageBox.Show("Поле пароль не заполнено!", "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            var dbUser = App.Connection.User.FirstOrDefault(x => x.Login == tbLogin.Text);
-
-            if (dbUser == null)
-            {
-                MessageBox.Show("Пользователя с таким логином не существует!", "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            if (!(Properties.Settings.Default.Blocker < DateTime.Now))
-            {
-                MessageBox.Show("Действует блокировка ввода пароля", "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            if (dbUser.Password != pbPassword.Password)
-            {
-                _passwordWrongEnterCount++;
-                if (_passwordWrongEnterCount >= _maxPasswordWrongEnterCount)
+                if (string.IsNullOrWhiteSpace(tbLogin.Text))
                 {
-                    Properties.Settings.Default.Blocker = DateTime.Now.AddMinutes(1);
-                    Properties.Settings.Default.Save();
-                    _passwordWrongEnterCount = 0;
+                    MessageBox.Show("Поле логин не заполнено!", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
                 }
-                MessageBox.Show("Неверный пароль!", "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+                if (string.IsNullOrEmpty(pbPassword.Password))
+                {
+                    MessageBox.Show("Поле пароль не заполнено!", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
 
-            if (chbRemember.IsChecked == true)
+                var dbUser = App.Connection.User.FirstOrDefault(x => x.Login == tbLogin.Text);
+
+                if (dbUser == null)
+                {
+                    MessageBox.Show("Пользователя с таким логином не существует!", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (!(Properties.Settings.Default.Blocker < DateTime.Now))
+                {
+                    MessageBox.Show("Действует блокировка ввода пароля", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (dbUser.Password != pbPassword.Password)
+                {
+                    _passwordWrongEnterCount++;
+                    if (_passwordWrongEnterCount >= _maxPasswordWrongEnterCount)
+                    {
+                        Properties.Settings.Default.Blocker = DateTime.Now.AddMinutes(1);
+                        Properties.Settings.Default.Save();
+                        _passwordWrongEnterCount = 0;
+                    }
+                    MessageBox.Show("Неверный пароль!", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (chbRemember.IsChecked == true)
+                {
+                    Properties.Settings.Default.Login = tbLogin.Text;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    Properties.Settings.Default.Login = "";
+                    Properties.Settings.Default.Save();
+                }
+
+                NavigationService.Navigate(new ClientsPage());
+            }
+            catch
             {
-                Properties.Settings.Default.Login = tbLogin.Text;
-                Properties.Settings.Default.Save();
+                MessageBox.Show("Ошибка", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-            NavigationService.Navigate(new ClientsPage());
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
